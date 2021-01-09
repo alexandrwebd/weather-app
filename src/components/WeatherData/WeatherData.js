046dd26e73645cf7d8c1c73aa87c0f6003a7ad35
Alexandr <alexandrwebd@gmail.com>
2021-01-09 12:22:20 +0200

    add class activeTemperature

--- src/components/WeatherData/WeatherData.js
+++ src/components/WeatherData/WeatherData.js
@@ -3,17 +3,18 @@ import classes from './WeatherData.module.css'
 import WeatherList from '../WeatherList/WeatherList'
 
 const WeatherData = (props) => {
-  const cls = []
+  const cls = [classes.WeatherGradus]
 
+  // если приходит температура добавляю класс со стилизацией
   if (props.temperature !== '') {
-    cls.push('WeatherGradus')
+    cls.push(classes.activeGradus)
   }
 
   return (
     <div className={classes.WeatherData}>
       <h3 className={classes.WeatherCity}>{props.city}</h3>
       <div className={classes.WeatherGradusWrapper}>
-        <p className={classes.WeatherGradus}>{props.temperature}</p>
+        <p className={cls.join(' ')}>{props.temperature}</p>
         <p>{props.description}</p>
       </div>
       <WeatherList weather={props.weather} />
